a473718b5b24c3528e15b21673d3f9646ecdae3c
Srunborath7 <srunborath7@gmail.com>
2026-05-28 10:57:52 +0700

    get chat by room id

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const server = http.createServer(app);
  */
 const io = new Server(server, {
   cors: {
-    origin: "*"
+    origin: "*",
+    methods: ["GET", "POST"]
   }
 });
 
--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -4,5 +4,6 @@ const chatController = require('../controllers/chatController');
 
 router.get('/', chatController.index);
 router.post('/', chatController.store);
+router.get('/room/:room_id', chatController.getByRoom);
 
 module.exports = router;
